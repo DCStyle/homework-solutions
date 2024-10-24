bcc4f3eef9d9bbbd916af52589cdb8b437c03d1a
dcstylexf <dcstylexf@gmail.com>
2024-10-25 04:00:51 +0700

    Disable sass

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,7 @@
 
 namespace Database\Seeders;
 
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use App\Models\Category;
-use App\Models\Permission;
-use App\Models\Role;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
