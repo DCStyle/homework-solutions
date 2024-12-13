4dcb24d4b7af6ca1436a4552126aff7e2408fdc3
dcstylexf <dcstylexf@gmail.com>
2024-12-14 02:21:40 +0700

    Update

--- resources/views/admin_layouts/admin.blade.php
+++ resources/views/admin_layouts/admin.blade.php
@@ -1,5 +1,5 @@
 <!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="overflow-hidden">
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -7,7 +7,6 @@
     @include('layouts.externalStylesheets')
 
     @vite('resources/css/app.css')
-    @vite('resources/css/admin.css')
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
