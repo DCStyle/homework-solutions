034c4abc73df2f8038e36c0626ff171454dd55ec
dcstylexf <dcstylexf@gmail.com>
2024-12-14 02:55:04 +0700

    Update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,7 @@
 namespace App\Providers;
 
 use App\Models\Category;
+use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -20,6 +21,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if(config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
+
         // Fetch only parent categories with their children
         $categories = Category::whereNull('parent_id')->with('children')->get();
 
