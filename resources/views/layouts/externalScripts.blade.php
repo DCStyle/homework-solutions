ff68769fbbaad09a61c3aa6c2d543fa7acc48d95
dcstylexf <dcstylexf@gmail.com>
2025-03-10 13:40:33 +0700

    Update

--- resources/views/layouts/externalScripts.blade.php
+++ resources/views/layouts/externalScripts.blade.php
@@ -5,3 +5,14 @@
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
 <script src="{{ asset('js/jquery.relativeTime.min.js') }}"></script>
+
+
+<script>
+    MathJax = {
+        tex: {
+            inlineMath: [['$', '$'], ['\\(', '\\)']],
+            displayMath: [['$$', '$$'], ['\\[', '\\]']]
+        }
+    };
+</script>
+<script src="https://cdn.jsdelivr.net/npm/mathjax@3/es5/tex-mml-chtml.js" async></script>
